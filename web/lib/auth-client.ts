f2c06c0cfa6ab94315a25b6fa2e0b7e51f7afc5b
Zainal Hasan <19884603+zhanang19@users.noreply.github.com>
2026-02-02 10:49:04 +0000

    [SP-051] Disable registration & script to create user

--- web/lib/auth-client.ts
+++ web/lib/auth-client.ts
@@ -1,4 +1,4 @@
-import { inferAdditionalFields } from 'better-auth/client/plugins'
+import { adminClient, inferAdditionalFields } from 'better-auth/client/plugins'
 import { createAuthClient } from 'better-auth/react'
 
 import { BETTER_AUTH_URL } from '@/constants/env'
@@ -6,5 +6,5 @@ import { type auth } from '@/lib/auth'
 
 export const authClient = createAuthClient({
   baseURL: BETTER_AUTH_URL,
-  plugins: [inferAdditionalFields<typeof auth>()],
+  plugins: [adminClient(), inferAdditionalFields<typeof auth>()],
 })
